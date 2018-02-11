aa2a32bfbceb2100b2f34ae488c29ad983abed3c
robo9k <robo@9k.lv>
2018-02-11 15:29:17 +0100

    Initial import

---
+++ examples/hello.rs
@@ -0,0 +1,56 @@
+#[macro_use]
+extern crate quake3_native_vm;
+// Needed for C FFI
+extern crate libc;
+// Needed to initialize internal `static mut` until const-fns are stable
+#[macro_use]
+extern crate lazy_static;
+
+use std::ffi::CString;
+
+struct HelloQuake3 {
+   syscall: Syscall,
+}
+
+use quake3_native_vm::*;
+
+/// See ioquake3's [game/g_public.h](https://github.com/ioquake/ioq3/blob/master/code/game/g_public.h)
+const G_ERROR: libc::intptr_t = 1;
+const GAME_INIT: libc::c_int = 0;
+const GAME_SHUTDOWN: libc::c_int = 1;
+
+impl QVM for HelloQuake3 {
+   fn dll_entry(syscall: Syscall) -> Box<HelloQuake3> {
+       Box::new(HelloQuake3 { syscall: syscall })
+   }
+
+   fn vm_main(&self,
+              command: libc::c_int,
+              _arg0: libc::c_int,
+              _arg1: libc::c_int,
+              _arg2: libc::c_int,
+              _arg3: libc::c_int,
+              _arg4: libc::c_int,
+              _arg5: libc::c_int,
+              _arg6: libc::c_int,
+              _arg7: libc::c_int,
+              _arg8: libc::c_int,
+              _arg9: libc::c_int,
+              _arg10: libc::c_int,
+              _arg11: libc::c_int)
+              -> libc::intptr_t {
+       match command {
+           GAME_INIT => {
+               (self.syscall)(G_ERROR, CString::new("Hello, World!").unwrap().as_ptr());
+               unreachable!()
+           }
+           GAME_SHUTDOWN => {
+               // Just return a dummy value here for clean shutdown
+               0
+           },
+           _ => panic!("Game command not implemented"),
+       }
+   }
+}
+
+native_qvm!(HelloQuake3);
